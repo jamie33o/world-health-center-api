b126e80efe36fa7f6a0a972cd93a0cdfd761b480
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-12-28 21:45:47 +0000

    Refactor: change api app name to products and move manage.py to root level

--- whc_api/settings.py
+++ whc_api/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.ApiConfig',
+    'products.apps.ApiConfig',
     'rest_framework',
 ]
 
